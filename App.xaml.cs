242b4e7cdaefaa8eb011f62951ff98f69dd05b9e
sebsg-spec <popasebi122@gmail.com>
2023-11-27 18:21:24 +0200

    New Lab

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,29 @@
-﻿namespace Popa_Sebastian_Ioan_Lab7
+﻿using SQLite;
+using Popa_Sebastian_Ioan_Lab7.Models;
+using System.IO;
+using Popa_Sebastian_Ioan_Lab7.Data;
+
+
+
+namespace Popa_Sebastian_Ioan_Lab7
+{ 
+public partial class App : Application
 {
-    public partial class App : Application
+    static ShoppingListDataBase database;
+    public static ShoppingListDataBase Database
     {
+        get
+        {
+            if (database == null)
+            {
+                database = new
+               ShoppingListDataBase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+               LocalApplicationData), "ShoppingList.db3"));
+            }
+            return database;
+        }
+    }
+
         public App()
         {
             InitializeComponent();
@@ -10,3 +32,4 @@
         }
     }
 }
+    
---
+++ ListPage.xaml.cs
@@ -0,0 +1,26 @@
+using Popa_Sebastian_Ioan_Lab7.Models;
+
+namespace Popa_Sebastian_Ioan_Lab7
+{
+
+    public partial class ListPage : ContentPage
+    {
+        public ListPage()
+        {
+            InitializeComponent();
+        }
+        async void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            var slist = (ShopList)BindingContext;
+            slist.Date = DateTime.UtcNow;
+            await App.Database.SaveShopListAsync(slist);
+            await Navigation.PopAsync();
+        }
+        async void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            var slist = (ShopList)BindingContext;
+            await App.Database.DeleteShopListAsync(slist);
+            await Navigation.PopAsync();
+        }
+    }
+}
