715d87879be918cd1bde19bf3a55e4892d6fc1af
sebsg-spec <popasebi122@gmail.com>
2023-12-12 17:25:35 +0200

    final

--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -23,13 +23,19 @@ namespace Popa_Sebastian_Ioan_Lab7
             base.OnAppearing();
             var shopl = (ShopList)BindingContext;
 
-            
+            var items = await App.Database.GetShopsAsync();
+            ShopPicker.ItemsSource = (System.Collections.IList)items;
+            ShopPicker.ItemDisplayBinding = new Binding("ShopDetails");
+
+
             listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
         }
         async void OnSaveButtonClicked(object sender, EventArgs e)
         {
             var slist = (ShopList)BindingContext;
             slist.Date = DateTime.UtcNow;
+            Shop selectedShop = (ShopPicker.SelectedItem as Shop);
+            slist.ShopID = selectedShop.ID;
             await App.Database.SaveShopListAsync(slist);
             await Navigation.PopAsync();
         }
